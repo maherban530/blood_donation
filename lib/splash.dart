be370ac3f41e8e3c7cdb319cb61fe83d2f359889
maherban <54341212+maherban530@users.noreply.github.com>
2021-08-27 17:43:56 +0530

    done

--- lib/splash.dart
+++ lib/splash.dart
@@ -11,7 +11,13 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Image.asset("images/log.png")),
+      body: Center(
+          child: Image.asset(
+        "images/log.png",
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        fit: BoxFit.cover,
+      )),
     );
   }
 }
